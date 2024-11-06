10d325ec73fa1dfab7f567f304ebafd6b2028c11
Temal Karunanayake <79495831+temalcode@users.noreply.github.com>
2024-11-06 08:17:01 +0530

    Add files via upload

---
+++ src/main/java/lk/leco/epc/EpcPortalApiApplication.java
@@ -0,0 +1,13 @@
+package lk.leco.epc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpcPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpcPortalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/leco/epc/EpcPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.leco.epc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpcPortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
